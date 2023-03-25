cb7d4294271129416f7be863597b3b2c1262d7c7
SoundhariyaSi <128037023+SoundhariyaSi@users.noreply.github.com>
2023-03-25 16:10:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Book from './Components/Book'
+function App() {
+  return (
+    <div>
+    <h1>Books</h1>
+    <Book name="Harry Potter and the Sorcerer's stone" author="J.K Rowling" />
+       <Book name="The Battle of the Labrynth" author="Rick Riordan" />
+      <Book name="Six of Crows" author="Leigh Bardugo" />
+      <Book name="Harry Potter and the Prisoner of Askaban" author="J.K Rowling" />
+      <Book name="Shadow and Bone" author="Leigh Bardugo" />
+
+    </div>
+  )
+}
+
+export default App
